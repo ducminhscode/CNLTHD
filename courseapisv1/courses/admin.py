e4d08a5bc2cfd2cadcc8cd388eb0947279f6efdb
Duc Minh <ducmin76@gmail.com>
2024-10-22 20:54:19 +0700

    bonus

--- courseapisv1/courses/admin.py
+++ courseapisv1/courses/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from courses.models import Category, Course
+from django.utils.safestring import mark_safe
+
+from courses.models import Category, Course, Lesson
 # Register your models here.
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['id', 'subject', 'active', 'created_data']
+    search_fields = ['subject', 'content']
+    list_filter = ['id','subject','created_data']
+    list_per_page = 1
+    list_editable = ['subject']
+    readonly_fields = ['avatar']
+
+    def avatar(self, lesson):
+        return mark_safe(f"<img src ='/static/{lesson.image.name}' width='200' />")
+
 
 admin.site.register(Category)
 admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
--- courseapisv1/courses/models.py
+++ courseapisv1/courses/models.py
@@ -13,11 +13,15 @@ class Category(models.Model):
 
 class BaseModel(models.Model):
     active = models.BooleanField(default=True)
-    created_data = models.DateTimeField(auto_now_add=True)
-    updated_data = models.DateTimeField(auto_now=True)
+    created_data = models.DateTimeField(auto_now_add=True, null=True)
+    updated_data = models.DateTimeField(auto_now=True,null=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-id']
 
 class Course(models.Model):
-    subject = models.TextField(max_length=255,null=False)
+    subject = models.CharField(max_length=255,null=False)
     description = models.TextField()
     image = models.ImageField(upload_to="courses/%Y/%m")
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -25,3 +29,16 @@ class Course(models.Model):
 
     def __str__(self):
         return self.subject
+
+    class Meta:
+        unique_together = ['subject', 'category']
+
+class Lesson(BaseModel):
+    subject = models.CharField(max_length=255, null=False)
+    content = models.TextField(null=False)
+    image = models.ImageField(upload_to="lessons/%Y/%m")
+    course = models.ForeignKey(Course,on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.subject
+
